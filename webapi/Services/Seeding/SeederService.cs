306fa95214140fa6f18e10d4c4dcfa62abb73aff
Hris <xrisko06@abv.bg>
2024-06-01 20:48:03 +0300

    Scrape ops.

--- webapi/Services/Seeding/SeederService.cs
+++ webapi/Services/Seeding/SeederService.cs
@@ -1,5 +1,6 @@
 ﻿using AngleSharp;
 using webapi.Data;
+using webapi.Data.Models;
 using static webapi.Common.GlobalConstants;
 
 namespace webapi.Services.Seeding
@@ -15,9 +16,51 @@ namespace webapi.Services.Seeding
             _browsingContext = new BrowsingContext(ScraperConfig);
         }
 
-        public Task SeedAsync()
+        public async Task SeedAsync()
         {
-            throw new NotImplementedException();
+            if (_context.Operators.Any()) return;
+
+            foreach (var (link, photo) in await GetOperatorLinks())
+            {
+                using var document = await _browsingContext.OpenAsync(link);
+                string icon = document.QuerySelector(".operator__header__icons__names img")!.GetAttribute("src")!;
+                string name = document.QuerySelector(".operator__header__icons__names h1")!.TextContent;
+                string side = document.QuerySelector(".operator__header__side__detail span")!.TextContent;
+
+                // list containing hp, speed and difficulty
+                var stats = document.QuerySelectorAll(".react-rater").Select(r => r.QuerySelectorAll(".is-active").Count()).ToList();
+
+                // Zero doesn't have his difficulty stat. Fuck you Ubisoft
+                if (stats.Count == 2) stats.Add(1);
+
+                (int hp, int speed, int difficulty) = (stats[0], stats[1], stats[2]);
+
+                var loadOutItems = document.QuerySelectorAll(".operator__loadout__category__items");
+
+                string? ability = null;
+                string? abilityPhoto = null;
+                if (loadOutItems.Count() == 4)
+                {
+                    ability = loadOutItems[3].QuerySelector("p")?.TextContent;
+                    abilityPhoto = loadOutItems[3].QuerySelector("img")?.GetAttribute("src");
+                }
+
+                var op = new Operator
+                {
+                    Name = name,
+                    Photo = photo,
+                    Icon = icon,
+                    Side = side,
+                    Health = hp,
+                    Speed = speed,
+                    Difficulty = difficulty,
+                    Ability = ability,
+                    AbilityPhoto = abilityPhoto
+                };
+
+                await _context.Operators.AddAsync(op);
+                await _context.SaveChangesAsync();
+            }
         }
 
         /// <returns>A dictionary with the operator link as key and photo as value</returns>
